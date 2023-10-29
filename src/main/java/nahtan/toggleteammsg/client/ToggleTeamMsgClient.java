f0c1cba7c7b94d70dde9a3369d19dc902072064b
Nathan Bateson <nathanjohnbateson@gmail.com>
2023-10-29 01:12:36 +0000

    Fixing Commit

--- src/main/java/nahtan/toggleteammsg/client/ToggleTeamMsgClient.java
+++ src/main/java/nahtan/toggleteammsg/client/ToggleTeamMsgClient.java
@@ -1,6 +1,12 @@
 package nahtan.toggleteammsg.client;
 
+import nahtan.toggleteammsg.commands.ToggleTeamMsgCmd;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
 
 public class ToggleTeamMsgClient implements ClientModInitializer {
     /**
@@ -8,6 +14,23 @@ public class ToggleTeamMsgClient implements ClientModInitializer {
      */
     @Override
     public void onInitializeClient() {
-
+        ClientCommandRegistrationCallback.EVENT.register(ToggleTeamMsgCmd::register);
+        ClientSendMessageEvents.ALLOW_CHAT.register((msg) -> {
+            if(!ToggleTeamMsgCmd.isToggled()){
+                return true;
+            }
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            if(player == null){
+                return false;
+            }
+            try {
+                String command = "/teammsg "+msg;
+                command = command.substring(1);
+                player.networkHandler.sendChatCommand(command);
+            } catch (Exception e) {
+                player.sendMessage(Text.of("shits fucked"));
+            }
+            return false;
+        });
     }
 }
--- src/main/java/nahtan/toggleteammsg/commands/ToggleTeamMsgCmd.java
+++ src/main/java/nahtan/toggleteammsg/commands/ToggleTeamMsgCmd.java
@@ -1,2 +1,32 @@
-package nahtan.toggleteammsg.commands;public class ToggleTeamMsgCmd {
+package nahtan.toggleteammsg.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.platform.fabric.FabricClientAudiences;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.minecraft.command.CommandRegistryAccess;
+
+public class ToggleTeamMsgCmd {
+    private static boolean isToggled = false;
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess) {
+        dispatcher.register(ClientCommandManager.literal("toggleteammsg").executes(ToggleTeamMsgCmd::execute));
+    }
+
+    private static int execute(CommandContext<FabricClientCommandSource> ctx) {
+        Audience client = FabricClientAudiences.of().audience();
+        if(isToggled){
+            isToggled = false;
+            client.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize("&#ff0000Toggled /teammsg (Disabled)"));
+        }else{
+            isToggled = true;
+            client.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize("&#00ff00Toggled /teammsg (Enabled)"));
+        }
+        return 0;
+    }
+    public static boolean isToggled(){
+        return isToggled;
+    }
 }
