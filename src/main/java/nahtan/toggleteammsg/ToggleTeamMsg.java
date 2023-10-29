c4962c92320051a188c320f5bd7430947584e52c
Nathan Bateson <nathanjohnbateson@gmail.com>
2023-10-29 01:05:43 +0000

    Initial commit Fabric 1.20.1

---
+++ src/main/java/nahtan/toggleteammsg/ToggleTeamMsg.java
@@ -0,0 +1,13 @@
+package nahtan.toggleteammsg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ToggleTeamMsg implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/nahtan/toggleteammsg/commands/ToggleTeamMsgCmd.java
@@ -0,0 +1,2 @@
+package nahtan.toggleteammsg.commands;public class ToggleTeamMsgCmd {
+}
